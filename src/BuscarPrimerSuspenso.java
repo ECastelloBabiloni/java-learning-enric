b3903367fbf6469cdd7ccb7bda9e06245db12259
Enric Castello <rixocb@gmail.com>
2026-07-14 19:52:03 +0200

    Ejercicios de Arrays, BuscarPrimerSuspenso

---
+++ src/BuscarPrimerSuspenso.java
@@ -0,0 +1,25 @@
+public class BuscarPrimerSuspenso {
+    public static void main(String[] args){
+        String[] alumnos = {"Ana", "Luis", "Marta", "Pedro", "Lucia"};
+        double[] notas = {7.5, 6.0, 9.2, 5.8, 8.7};
+
+        int posicion = posicionPrimerSuspendido(notas);
+
+        if (posicion == -1){
+            System.out.println("No hay alumnos suspendidos");
+        } else {
+            System.out.println("EL primer alumno suspendido es " + alumnos[posicion] + " con una nota de " + notas[posicion]);
+        }
+
+    }
+    public static int posicionPrimerSuspendido(double[] notas){
+        int primerSuspendido = -1;
+        for (int i = 0; i < notas.length; i++){
+            if (notas[i] < 5){
+                primerSuspendido = i;
+                break;
+            }
+        }
+        return primerSuspendido;
+    }
+}
