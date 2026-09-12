2c917e8adf93db33130df6d3ecaa5daf14ec845e
Enric Castello <rixocb@gmail.com>
2026-09-12 10:59:35 +0200

    Permite varias compras y muestra importes con dos decimales

--- src/TiendaApp.java
+++ src/TiendaApp.java
@@ -1,64 +1,71 @@
 import java.util.Scanner;
 public class TiendaApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
         // PROVEEDORES
 
         Proveedor babiloni = new Proveedor("Babiloni", "Borriol");
-        Proveedor fMarisa = new Proveedor("FrutasMarisa","Castellon");
+        Proveedor fMarisa = new Proveedor("FrutasMarisa", "Castellon");
 
         // PRODUCTOS
 
-        Producto harina = new Producto("Harina",2.5,3,babiloni);
-        Producto tomate = new Producto("Tomate", 1.2,4,fMarisa);
-        Producto aceite = new Producto("Aceite", 9.2,5,babiloni);
+        Producto harina = new Producto("Harina", 2.5, 3, babiloni);
+        Producto tomate = new Producto("Tomate", 1.2, 4, fMarisa);
+        Producto aceite = new Producto("Aceite", 9.2, 5, babiloni);
 
         //Array tiendaApp e Inventario
 
         Producto[] productosTiendaApp = {harina, tomate, aceite};
         Inventario inventarioTiendaApp = new Inventario(productosTiendaApp);
 
-        // INPUT?
+        String continuar = "s";
+        double importeTotal = 0;
 
-        inventarioTiendaApp.mostrarProductosConProveedor();
-        System.out.println("¿Que producto estas buscando?");
-        String nombreBuscado = teclado.nextLine();
+        while (continuar.equalsIgnoreCase("s")) {
 
-        // LLAMADAS
+            // INPUT?
+            inventarioTiendaApp.mostrarProductosConProveedor();
+            System.out.println("¿Que producto estas buscando?");
+            String nombreBuscado = teclado.nextLine();
 
-        Producto productoBuscado = inventarioTiendaApp.buscarProductoPorNombre(nombreBuscado);
+            // LLAMADAS
 
-        if (productoBuscado != null){
-            System.out.println("El proveedor de "+ productoBuscado.getNombre() +" es " + productoBuscado.getProveedor().getNombre());
-            System.out.println("El precio del producto "+ productoBuscado.getNombre() +" es de  " + productoBuscado.getPrecio()+ " Euros, y tenemos "+productoBuscado.getCantidad()+ " unidades en el Stock");
-            // VENTAS
-            System.out.println("¿Cuantas unidades quiere comprar?");
+            Producto productoBuscado = inventarioTiendaApp.buscarProductoPorNombre(nombreBuscado);
 
-            if (teclado.hasNextInt()){
-                int vender = teclado.nextInt();
-                
-                if (productoBuscado.venderUnidades(vender)){
-                    double importeVenta = vender * productoBuscado.getPrecio();
-                    System.out.println("La compra se ha realizado correctamente, ahora quedan " + productoBuscado.getCantidad()+ " unidades de "+ productoBuscado.getNombre());
-                    System.out.println("El importe total de la compra es "+ importeVenta + " Euros");
-                }else{
-                    if (vender > 0){
-                        System.out.println("No hay stock suficiente");
-                    }else{
-                        System.out.println("Las unidades introducidas han de ser mayores que 0");
+            if (productoBuscado != null) {
+                System.out.println("El proveedor de " + productoBuscado.getNombre() + " es " + productoBuscado.getProveedor().getNombre());
+                System.out.println("El precio del producto " + productoBuscado.getNombre() + " es de  " + productoBuscado.getPrecio() + " Euros, y tenemos " + productoBuscado.getCantidad() + " unidades en el Stock");
+                // VENTAS
+                System.out.println("¿Cuantas unidades quiere comprar?");
+
+                if (teclado.hasNextInt()) {
+                    int vender = teclado.nextInt();
+
+                    if (productoBuscado.venderUnidades(vender)) {
+                        double importeVenta = vender * productoBuscado.getPrecio();
+                        importeTotal += importeVenta;
+                        System.out.println("La compra se ha realizado correctamente, ahora quedan " + productoBuscado.getCantidad() + " unidades de " + productoBuscado.getNombre());
+                        System.out.printf("El importe del articulo es de %.2f Euros%n",importeVenta);
+
+                    } else {
+                        if (vender > 0) {
+                            System.out.println("No hay stock suficiente");
+                        } else {
+                            System.out.println("Las unidades introducidas han de ser mayores que 0");
+                        }
+
                     }
-
+                } else {
+                    System.out.println("Las unidades de compra han de ser numeros enteros");
                 }
-            }else{
-                System.out.println("Las unidades de compra han de ser numeros enteros");
+            } else {
+                System.out.println("Producto no encontrado");
             }
-        }else{
-            System.out.println("Producto no encontrado");
+            System.out.println("¿Quieres seguir comprando? s/n");
+            teclado.nextLine();
+            continuar = teclado.nextLine();
         }
-
-
-
-
+        System.out.printf("EL importe total de la compra es de %.2f Euros%n", importeTotal);
     }
 }
